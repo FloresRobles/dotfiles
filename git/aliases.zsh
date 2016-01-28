2b2e5d4124d9abc5a3edb9663942f5d2da83b98c
Javier Flores <jeflro@gmail.com>
2016-01-27 18:42:40 -0800

    git dev && git dev branch

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,5 @@
 # specific aliases
-alias gdev="git checkout develop; git pull"
+alias gdev="$DZSH/bin/git-dev"
 alias gclean="gdev; git branch --merged | grep -v develop | xargs -n 1 git branch -d"
 alias gmerge-to="$DZSH/bin/git-merge-to"
 alias gdb="$DZSH/bin/git-dev-branch"
