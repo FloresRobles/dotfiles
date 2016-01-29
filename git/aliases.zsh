8d0b95780e9c605a7fd8409500595c4594f9d016
Javier Flores <jeflro@gmail.com>
2016-01-28 16:11:34 -0800

    updated with dot-push-changes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,5 +1,4 @@
 # specific aliases
 alias gdev="$DZSH/bin/git-dev"
-alias gclean="gdev; git branch --merged | grep -v develop | xargs -n 1 git branch -d"
 alias gmerge-to="$DZSH/bin/git-merge-to"
 alias gdb="$DZSH/bin/git-dev-branch"
