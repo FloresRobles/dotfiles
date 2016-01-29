c356686b982b5a8013982ed764973a11ffb05c41
Javier Flores <jeflro@gmail.com>
2016-01-29 11:42:55 -0800

    updated with dot-push-changes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,4 +1,5 @@
 # specific aliases
 alias gdev="$DZSH/bin/git-dev"
-alias gmerge-to="$DZSH/bin/git-merge-to"
 alias gdb="$DZSH/bin/git-dev-branch"
+alias gmerge-to="$DZSH/bin/git-merge-to"
+alias gps='git push --set-upstream origin $(git_current_branch)'
