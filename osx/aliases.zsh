04f7011d1dd65024b57034f7317d91c5dd20b9e0
Javier Flores <jeflro@gmail.com>
2016-02-16 22:32:45 -0800

    not working

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,3 +1,2 @@
-alias wipe-dock="defaults write com.apple.dock persistent-apps -array"
 alias hide-all="defaults write com.apple.finder AppleShowAllFiles -bool NO; killall Finder"
 alias show-all="defaults write com.apple.finder AppleShowAllFiles -bool YES; killall Finder"
