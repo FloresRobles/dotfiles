e845948a447bc7a256bc95d163cff7ca5f84dd71
Javier Flores <jeflro@gmail.com>
2016-02-16 22:34:04 -0800

    edit-dot

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,2 +1,3 @@
 alias hide-all="defaults write com.apple.finder AppleShowAllFiles -bool NO; killall Finder"
 alias show-all="defaults write com.apple.finder AppleShowAllFiles -bool YES; killall Finder"
+alias edit-dot="atom ~./dotfiles"
