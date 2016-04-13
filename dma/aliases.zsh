2d5f82ce9dba08c9fbe0e8ef76d15c43c1914424
Javier Flores <jeflro@gmail.com>
2016-04-13 14:41:37 -0700

    slackcat

--- dma/aliases.zsh
+++ dma/aliases.zsh
@@ -2,4 +2,3 @@
 alias dma-artifacts="aws s3 sync s3://dma-ios-artifacts $HOME/Code/dma/s3artifacts"
 alias dma="cd $HOME/Code/dma/dma-ios"
 alias dmamr="gps; gitlab-merge-request 8"
-alias dmasbd="java -jar $HOME/.dotfiles/dma/jenkins-cli.jar -s http://seg-rm-mmba-2.studio.disney.com:8080 build -sv 'DMA iOS Debug'"
