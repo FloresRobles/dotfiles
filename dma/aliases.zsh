8b911a705737c0c2022414b00798c423acfb78a7
Javier Flores <jeflro@gmail.com>
2016-04-13 15:28:48 -0700

    fix route

--- dma/aliases.zsh
+++ dma/aliases.zsh
@@ -2,4 +2,4 @@
 alias dma="cd $HOME/Code/dma/dma-ios"
 alias dmamr="gps; gitlab-merge-request 8"
 alias dma-artifacts="aws s3 sync s3://dma-ios-artifacts $HOME/Code/dma/s3artifacts"
-alias dma-artifacts-list="ls -t -1 $PWD/*.* $HOME/Code/dma/s3artifacts | head -n3"
+alias dma-artifacts-list="ls -t -1 $HOME/Code/dma/s3artifacts/*.* $HOME/Code/dma/s3artifacts | head -n3"
--- dma/functions.zsh
+++ dma/functions.zsh
@@ -3,6 +3,14 @@ dma-artifacts-slack() {
   then
     echo "Usage: dma-artifacts-slack <channel/user>"
   else
-    dma-artifacts-list | xargs -L 1 slackcat --channel $1
+    echo "Syncing the files...\n"
+    dma-artifacts
+    read -p "$(dma-artifacts-list)\nDoes this looks ok?" -n 1 -s char
+    if [ "$char" = "y" ]
+    then
+      dma-artifacts-list | xargs -L 1 slackcat --channel $1
+    else
+      echo "You better fix it!\n"
+    fi
   fi
 }
