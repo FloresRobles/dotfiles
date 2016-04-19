487537500316ef7ded3d8e5cdfe4919bed6311be
Javier Flores <jeflro@gmail.com>
2016-04-19 15:58:42 -0700

    symbolicatecrash

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,2 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+alias symbolicatecrash="/Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework/Versions/A/Resources/symbolicatecrash"
