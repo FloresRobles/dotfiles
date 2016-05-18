c96ac435b49b5a63a892d6af6a160766f91d287f
Javier Flores <jeflro@gmail.com>
2016-05-18 10:38:35 -0700

    force

--- dma/functions.zsh
+++ dma/functions.zsh
@@ -1,16 +1,21 @@
 dma-artifacts-slack() {
   if [ "$1" = "" ]
   then
-    echo "Usage: dma-artifacts-slack <channel/user>"
+    echo "Usage: dma-artifacts-slack <channel/user> <force/>"
   else
     echo "Syncing the files...\n"
     dma-artifacts
-    echo "$(dma-artifacts-list)\nDoes this looks ok?"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes ) dma-artifacts-list | xargs -L 1 slackcat --channel $1; break;;
-            No ) echo "You better fix it!\n"; break;;
-        esac
-    done
+    if [ "$2" = "force" ]
+    then
+      dma-artifacts-list | xargs -L 1 slackcat --channel $1;
+    else
+      echo "$(dma-artifacts-list)\nDoes this looks ok?"
+      select yn in "Yes" "No"; do
+          case $yn in
+              Yes ) dma-artifacts-list | xargs -L 1 slackcat --channel $1; break;;
+              No ) echo "You better fix it!\n"; break;;
+          esac
+      done
+    fi
   fi
 }
